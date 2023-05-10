7c57682718b9f2524aa957eaa877a1e4c21364d3
Ishi01 <wspain@gmail.com>
2023-05-10 15:15:07 +0800

    Added functionality to edit the Type_of_Breach column to specification Additionally added further error checking to check that number of columns is correct TODO: Add error checking for column formats

--- preprocess.sh
+++ preprocess.sh
@@ -12,14 +12,28 @@ then
 	exit 1
 fi
 
-#Add months column to file
+#Check if file is in correct .tsv format
+if [[ $filename != *".tsv"* ]];
+then
+	echo "File must be in the .tsv format"
+	exit 1
+fi
 
-#Print all date info from column
-#remove everthing after the first slash, since the month is
-#awk -F '\t' 'BEGIN{NR==1 {print }}'NR>1 { print $4 }' $filename | cut -f1 -d'/' | sed 's/^0*//' > tmp 
+#Make sure file has correct number of columns
+numFields=$(awk -F '\t' 'NR<2{print NF}' $filename)
 
-#Remove location and summary columns from data
-awk -F '\t' '{print $1"\t"$2"\t"$3"\t"$4"\t"$5}' $filename > temp.tsv
+if [ $numFields != 7 ];
+then
+	echo "File must have correct number of columns (7)"
+	exit 1
+fi
+
+
+
+
+#Remove location and summary columns from data (and temporarily remove type of breach so it can be edited and re-added)
+awk -F '\t' '{print $1"\t"$2"\t"$3"\t"$4}' $filename > temp.tsv
+
 
 #Extract the date information that we want from the columns
 
@@ -27,14 +41,16 @@ awk -F '\t' '{print $1"\t"$2"\t"$3"\t"$4"\t"$5}' $filename > temp.tsv
 #Print the column with dates, cut off everything after the first /, remove leading zeroes and save to temporary file
 awk -F '\t' 'BEGIN{print "Month"} NR>1 { print $4 }' $filename | cut -f1 -d'/' | sed 's/^0*//' > monthsTemp.tsv
 
-#Extract year info (leading zeroes not kept)
+#Extract year info
 awk -F '\t' 'BEGIN{print "Year"} NR>1 { print $4 }' $filename | cut -f1 -d'-' | sed 's/.*\///' > yearsTemp1.tsv
+
+#Extract Type_of_Breach info, remove everything after the first / or , and save it to temp file
+awk -F '\t' 'BEGIN{print "Type_of_Breach"} NR>1 { print $5 }' $filename | cut -f1 -d'/' | cut -f1 -d',' > typeOfBreachTemp.tsv
 
 #read contents of yearsTemp into array to be iterated over
 readarray -t year_array < yearsTemp1.tsv
 
 #Iterate over each item
-index=0
 for i in "${!year_array[@]}";
 do
 	#declare value as num in current index
@@ -62,4 +78,11 @@ printf "%s\n" "${year_array[@]}" > yearsTemp2.tsv
 
 
 #append month data file with the overall data file using paste
-paste temp.tsv monthsTemp.tsv > temp3.tsv
+paste temp.tsv typeOfBreachTemp.tsv monthsTemp.tsv yearsTemp2.tsv > Cyber_Security_Breaches_clean.tsv
+
+#remove all of the temp files
+rm temp.tsv
+rm typeOfBreachTemp.tsv
+rm monthsTemp.tsv
+rm yearsTemp1.tsv
+rm yearsTemp2.tsv
