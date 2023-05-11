f08e283cde631cb75d227e6a85c5a3ec812e14cc
Ishi01 <wspain@gmail.com>
2023-05-11 11:52:21 +0800

    Added ability to read count and months from cleaned file and save that info to an array for processing

--- breaches_per_month.sh
+++ breaches_per_month.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#Set filename input to variable
+
+filename=$1
+
+#ADD ERROR CHECKING LATER
+
+#Create month name and count arrays
+awk -F '\t' 'NR>1 {print $6}' $filename > monthsTemp.tsv
+awk -F '\t' 'NR>1 {print $3}' $filename > countTemp.tsv
+
+#read those values into arrays to be iterated over
+readarray -t months_array < monthsTemp.tsv
+readarray -t count_array < countTemp.tsv
